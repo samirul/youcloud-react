aea1640b19668b8a2c3b9f6cca44e0745885312c
Samirul Haque <samirulhaque08@gmail.com>
2024-02-22 03:10:16 +0900

    added react-router-dom

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -15,7 +15,7 @@ const Menu = () => {
                   <Navbar.Collapse id="basic-navbar-nav">
                       <Nav className="me-auto">
                           <Nav.Link href="#home">Home</Nav.Link>
-                          <Nav.Link href="#link">Link</Nav.Link>
+                          <Nav.Link href="/search/location">Link</Nav.Link>
                           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                               <NavDropdown.Item href="#action/3.2">
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
 import Search from '../components/Search'
 import Menu from '../components/Menu'
+import {
+  HashRouter as Router,
+  // BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Menu/>
-    <Search/>
+     <Router>
+        <Menu/>
+        <Routes> 
+            <Route path = "/search/location" exact element ={<Search/>}/>
+        </Routes>
+    </Router>
     </>
   )
 }
