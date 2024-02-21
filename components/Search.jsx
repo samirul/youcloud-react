523cd1a23926bf09240150a5f7590aaa7e741289
Samirul Haque <samirulhaque08@gmail.com>
2024-02-22 02:25:24 +0900

    first commit

---
+++ components/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FaSearch } from "react-icons/fa";
+
+const Search = () => {
+  return (
+    <div>
+          <div className="wrap-one">
+              <div className="search-one">
+                  <input type="text" className="searchTerm-one" placeholder=""/>
+                      <button type="submit" className="searchButton-one">
+                        <FaSearch />
+                      </button>
+              </div>
+          </div>
+    </div>
+  )
+}
+
+export default Search
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Search from '../components/Search'
+import Menu from '../components/Menu'
+
+function App() {
+  return (
+    <>
+    <Menu/>
+    <Search/>
+    </>
+  )
+}
+
+export default App
